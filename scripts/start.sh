70e9916e0b2d48a5ced2694e6da02d41c5b0c1cb
Steve Johnson <steve.johnson@qiotec.com>
2016-05-19 07:37:46 -0500

    Reorganize project

--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nohup java -Dconfig.file=application.conf -Dconfig.trace=loads -jar diyha_station.jar >/dev/null 2>&1 & PID=$!
-echo $PID > diyha_station.pid
--- scripts/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PID=$(cat diyha_station.pid)
-kill $PID
-rm -rf diyha_station.pid
